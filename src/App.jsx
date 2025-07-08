164b5ac03c1c4dbbd7ee06f9897d9e1866168246
Robin San Martin <robin.sanmartin.venegas@ciisa.cl>
2025-07-07 22:11:35 -0400

    added label

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>GitHub + Vite</h1>
+      <label>asdasd</label>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
